cbc85222188cd0fa8dd3ea00ce430a70cc49df69
Kyle Hartmann <Kyle.Hartmann@finastra.com>
2021-02-15 22:12:57 -0600

    Create bare minimum layout

---
+++ acorn-and-oak-photography/src/components/header/header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+
+function Header() {
+  return (
+    <header className="horizontal-header">
+        <h2>Acorn and Oak Photography</h2>
+    </header>
+  );
+}
+
+export default Header;

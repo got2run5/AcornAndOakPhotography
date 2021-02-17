4124e4d89b57f88e0cecc81f46272b2e7533a239
Kyle Hartmann <Kyle.Hartmann@finastra.com>
2021-02-17 00:10:56 -0600

    Add left nav hamburger menu and nav links.  Some branding

--- acorn-and-oak-photography/src/components/header/header.tsx
+++ acorn-and-oak-photography/src/components/header/header.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './header.css';
+import logo from './../../acorn.svg';
+import { faFacebookF, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Header() {
   return (
     <header className="horizontal-header">
+      <div>
+        <div>
+          <img className="logo" src={logo} alt="Acorn And Oak Logo" />
+        </div>
         <h2>Acorn and Oak Photography</h2>
+      </div>
+      <div className="social-media-container">
+        <FontAwesomeIcon icon={faFacebookF} size="lg"/>
+        <FontAwesomeIcon icon={faTwitter} size="lg"/>
+        <FontAwesomeIcon icon={faInstagram} size="lg"/>
+      </div>
     </header>
   );
 }
--- acorn-and-oak-photography/src/components/left-nav/left-nav.tsx
+++ acorn-and-oak-photography/src/components/left-nav/left-nav.tsx
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './left-nav.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { faBars, faHome, faCamera, faAddressCard, faFileInvoiceDollar, faImages } 
+from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function LeftNav() {
+  const [isExpanded, setExpanded] = useState(false);
+  let navItemClass = "nav-close";
+  navItemClass = isExpanded ? "nav-open" : "nav-close";
+
   return (
     <aside className="left-nav">
-        left nav
+      <div className="hamburger-container">
+        <button className="hamburger-menu" onClick={() => setExpanded(!isExpanded)}>
+          <FontAwesomeIcon icon={faBars} size="3x"/>
+        </button>
+      </div>
+      <Router>
+        <Link to="/">
+          <div className={navItemClass} onClick={() => setExpanded(false)}>
+            <FontAwesomeIcon icon={faHome} size="lg"/>
+            <span>Home</span>
+          </div>
+        </Link>
+        <Link to="/my-work">
+          <div className={navItemClass} onClick={() => setExpanded(false)}>
+            <FontAwesomeIcon icon={faCamera} size="lg"/>
+            <span>My Work</span>
+          </div>
+        </Link>
+        <Link to="/about-me">
+          <div className={navItemClass} onClick={() => setExpanded(false)}>
+            <FontAwesomeIcon icon={faAddressCard} size="lg"/>
+            <span>About Me</span>
+          </div>
+        </Link>
+        <Link to="/pricing">
+          <div className={navItemClass} onClick={() => setExpanded(false)}>
+            <FontAwesomeIcon icon={faFileInvoiceDollar} size="lg"/>
+            <span>Pricing</span>
+          </div>
+        </Link>
+        <Link to="/client-galleries">
+          <div className={navItemClass} onClick={() => setExpanded(false)}>
+            <FontAwesomeIcon icon={faImages} size="lg"/>
+            <span>Client Galleries</span>
+          </div>
+        </Link>
+      </Router>
     </aside>
   );
 }
